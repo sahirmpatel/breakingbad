7a141e5054499e62000c6483fc61391b728cf07e
Sahir Patel <sahirmpatel@gmail.com>
2020-07-14 07:34:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Header from "./components/ui/Header";
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios.get(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+    };
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
