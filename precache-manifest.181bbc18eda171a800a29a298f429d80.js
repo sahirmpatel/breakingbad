bf55a2c6fd6bcabc75ab416a078ee693cad78d78
Sahir Patel <sahirmpatel@gmail.com>
2020-07-14 09:02:16 +0530

    Updates

---
+++ precache-manifest.181bbc18eda171a800a29a298f429d80.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9079e3e4e2eab8f4f6cbfb7f6048b0",
+    "url": "/breakingbad/index.html"
+  },
+  {
+    "revision": "90dbcaaafc0a8428c0aa",
+    "url": "/breakingbad/static/css/main.a7ae93a9.chunk.css"
+  },
+  {
+    "revision": "9b4f97a8d7005de2e3fb",
+    "url": "/breakingbad/static/js/2.11b2aaec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breakingbad/static/js/2.11b2aaec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90dbcaaafc0a8428c0aa",
+    "url": "/breakingbad/static/js/main.a3d59f2b.chunk.js"
+  },
+  {
+    "revision": "ea2d3b5d3b339cb26b3a",
+    "url": "/breakingbad/static/js/runtime-main.e09d8a2d.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breakingbad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breakingbad/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breakingbad/static/media/spinner.235b9b3e.gif"
+  }
+]);
